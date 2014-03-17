2aa1de48c0b43b33b4ddd9007b270671b3820c9e
ffuis-aljosa <Korisnik@Korisnik-PC>
2014-03-17 13:45:17 +0100

    - Započeli program; - Bez progress bar-a;

---
+++ src/minidowloader/MiniDowloader.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minidowloader;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class MiniDowloader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NasProzor np = new NasProzor();
+        np.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        np.setVisible(true);
+    }
+}
